10ab1282ea77d97b275bc635a1e848d09adb45cb
Jeffrey | BOOOM Digital <jeffrey@booomdigital.nl>
2021-12-30 15:23:29 +0100

    whatsapp and mail links swap'

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,8 +6,8 @@
                 target="_blank"
                 >LINKEDIN</a
             >
-            <a href="https://wa.me/031682516209" target="_blank">EMAIL</a>
-            <a href="mailto:sevinga@protonmail.com" target="_blank">WHATSAPP</a>
+            <a href="mailto:sevinga@protonmail.com" target="_blank">EMAIL</a>
+            <a href="https://wa.me/031682516209" target="_blank">WHATSAPP</a>
         </div>
         <div class="footer-foot flex justify-between items-center">
             <div class="foot-left">
